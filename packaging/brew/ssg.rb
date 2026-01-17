fdb4943e3ab8e0899fab0aeae66e21c9463724a6
spagu <email@sremail.pl>
2026-01-17 21:14:18 +0000

    Release v1.3.4: Install WebP tools automatically in GitHub Action

--- packaging/brew/ssg.rb
+++ packaging/brew/ssg.rb
@@ -5,7 +5,7 @@
 class Ssg < Formula
   desc "Fast static site generator written in Go"
   homepage "https://github.com/spagu/ssg"
-  version "1.3.3"
+  version "1.3.4"
   license "BSD-3-Clause"
 
   on_macos do
--- packaging/rpm/ssg.spec
+++ packaging/rpm/ssg.spec
@@ -1,5 +1,5 @@
 Name:           ssg
-Version:        1.3.3
+Version:        1.3.4
 Release:        1%{?dist}
 Summary:        Fast static site generator written in Go
 
