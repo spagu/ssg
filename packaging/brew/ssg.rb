3c930945d4eaea60165a396181b46cbed473f663
Pastor <email@sremail.pl>
2026-07-21 15:22:46 +0100

    chore(release): bump version to 1.8.9

--- packaging/brew/ssg.rb
+++ packaging/brew/ssg.rb
@@ -9,27 +9,27 @@
 class Ssg < Formula
   desc "Fast static site generator written in Go"
   homepage "https://github.com/spagu/ssg"
-  version "1.8.8"
+  version "1.8.9"
   license "BSD-3-Clause"
 
   on_macos do
     on_arm do
-      url "https://github.com/spagu/ssg/releases/download/v1.8.8/ssg-darwin-arm64.tar.gz"
+      url "https://github.com/spagu/ssg/releases/download/v1.8.9/ssg-darwin-arm64.tar.gz"
       sha256 "339168be6005853362be1de8c9a38940186b6da17e9a67a3b161f92130e26c12"
     end
     on_intel do
-      url "https://github.com/spagu/ssg/releases/download/v1.8.8/ssg-darwin-amd64.tar.gz"
+      url "https://github.com/spagu/ssg/releases/download/v1.8.9/ssg-darwin-amd64.tar.gz"
       sha256 "427c02b3ef85449a45eee8990da0afefda59656e0ca8c51f0140fbb9bea700e9"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/spagu/ssg/releases/download/v1.8.8/ssg-linux-arm64.tar.gz"
+      url "https://github.com/spagu/ssg/releases/download/v1.8.9/ssg-linux-arm64.tar.gz"
       sha256 "b0ff8632d19b4d55990f3fd94f089a89690c18a1e6bd9d6cfdd8123afa89cffd"
     end
     on_intel do
-      url "https://github.com/spagu/ssg/releases/download/v1.8.8/ssg-linux-amd64.tar.gz"
+      url "https://github.com/spagu/ssg/releases/download/v1.8.9/ssg-linux-amd64.tar.gz"
       sha256 "bfd9c3e5e31a5e676f57105dc604fb74410f0022b86df3d35d6b0e239987ffff"
     end
   end
